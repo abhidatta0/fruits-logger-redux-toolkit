b26cf1c587bf86d97d6a8798ab43a6d2bc1bba90
Abhirup <abhidatta146@gmail.com>
2022-02-19 21:20:59 +0530

    basic redux setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import store from './store';
+import {Provider} from 'react-redux';
 
 function App() {
   return (
+    <Provider store={store}>
     <div>
       Hello
     </div>
+    </Provider>
   );
 }
 
